7d0a3e8114cbfe118a3b27b78e5026bd8ed0518a
Greg Hurrell <greg@hurrell.net>
2019-01-20 22:47:26 +0100

    Add very basic dedent tests

---
+++ packages/dedent/src/__tests__/index-test.js
@@ -0,0 +1,23 @@
+import dedent from '..';
+
+describe('dedent()', () => {
+  it('dedents based on the smallest existing indent', () => {
+    expect(dedent`
+      {
+        foo
+      }
+    `).toBe('{\n  foo\n}');
+  });
+
+  it('ignores empty lines', () => {
+    expect(dedent`
+      {
+        foo
+      }
+
+      {
+        bar
+      }
+    `).toBe('{\n  foo\n}\n\n{\n  bar\n}');
+  });
+});
