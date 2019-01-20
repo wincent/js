12a0b057546e76cd90c04dd11c4455ec184b4ba5
Greg Hurrell <greg@hurrell.net>
2019-01-20 23:34:41 +0100

    Format JS config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-      "flowtype"
-    ],
-    "rules": {
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-    root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['flowtype'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  root: true,
 };
--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,7 @@ module.exports = function(api) {
   api.cache(false);
 
   return {
-    ignore: [
-      '/__tests__/',
-    ],
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-flow",
-    ],
+    ignore: ['/__tests__/'],
+    presets: ['@babel/preset-env', '@babel/preset-flow'],
   };
 };
