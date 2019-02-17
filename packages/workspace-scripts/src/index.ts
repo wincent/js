0564eef3390cf72ff18b4435228978740406d4c7
Greg Hurrell <greg@hurrell.net>
2019-02-17 10:15:48 +0100

    Colorize workspace-scripts output

--- packages/workspace-scripts/src/index.ts
+++ packages/workspace-scripts/src/index.ts
@@ -28,9 +28,25 @@ const SUBCOMMANDS = {
   typecheck,
 };
 
-function print(output: string) {
-  process.stdout.write(`${output}\n`);
-}
+const RED = '\x1b[31m';
+const RESET = '\x1b[0m';
+const YELLOW = '\x1b[33m';
+
+const print = Object.assign(
+  (output: string) => {
+    process.stdout.write(`${output}\n`);
+  },
+  {
+    red(output: string) {
+      print(RED + output + RESET);
+    }
+  },
+  {
+    yellow(output: string) {
+      print(YELLOW + output + RESET);
+    }
+  }
+);
 
 function usage() {
   print('Usage: workspace-scripts SUBCOMMAND... [PACKAGE...] [[--] ARG...]');
@@ -43,7 +59,7 @@ function usage() {
 
 function bail(message?: string): never {
   if (message) {
-    print(message);
+    print.red(message);
   }
   return process.exit(1);
 }
@@ -95,7 +111,7 @@ function validatePackages(packages: string[], root: string): void {
   });
 
   if (badPackages.length) {
-    badPackages.forEach(pkg => print(`Package ${pkg} does not exist`));
+    badPackages.forEach(pkg => print.red(`Package ${pkg} does not exist`));
     bail();
   }
 }
@@ -264,7 +280,7 @@ function typecheckTS(packages: string[], extraArgs: string[]) {
 function checkTypecheckPackages(packages: string[]) {
   const {length} = packages;
   if (length) {
-    print(
+    print.yellow(
       `info: ignoring package argument${length > 1 ? 's' : ''} (${packages.join(
         ', ',
       )}) - typechecking cannot be scoped to individual packages`,
