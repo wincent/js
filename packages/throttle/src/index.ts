5d403acba7fa0f423fa998b3143737461a8d71cb
Greg Hurrell <greg@hurrell.net>
2019-08-05 22:54:18 +0200

    chore: import throttle implementation and tests from Corpus repo
    Not valid TypeScript yet (these files were Flow and will need to be
    ported).

--- packages/throttle/src/index.ts
+++ packages/throttle/src/index.ts
@@ -3,6 +3,33 @@
  * @license MIT
  */
 
-export default function throttle() {
-  // ...
+/**
+ * Throttle implementation that fires on the leading and trailing edges.
+ * If a call comes in when a pending call is yet to be processed, it replaces
+ * the pending call.
+ */
+export default function throttle<TArgs: Iterable<mixed>>(
+  fn: (...TArgs) => void,
+  interval: number,
+): (...TArgs) => void {
+  let timeout = null;
+  let last = null;
+  return function() {
+    const args: TArgs = arguments;
+    const context = this;
+    const now = Date.now();
+    if (timeout === null) {
+      timeout = setTimeout(() => (timeout = null), interval);
+      last = now;
+      fn.apply(context, args);
+    } else {
+      const remaining = Math.max(last + interval - now, 0);
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        timeout = setTimeout(() => (timeout = null), interval);
+        last = now;
+        fn.apply(context, args);
+      }, remaining);
+    }
+  };
 }
